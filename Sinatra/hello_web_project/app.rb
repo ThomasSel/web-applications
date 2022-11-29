93da55044f0e3842cb58640d17cbbfca7aa6f0d1
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-11-29 10:20:22 +0000

    Adds extra routes [FROM PAIRING ON 28/11]

--- Sinatra/hello_web_project/app.rb
+++ Sinatra/hello_web_project/app.rb
@@ -7,4 +7,41 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  not_found do
+    status 404
+    return "Sorry! We couldn't find this post."
+  end
+
+  get '/' do
+    name = params[:name]
+    name = "Stranger" if name.nil?
+    return "Hello, #{name}!"
+  end
+
+  post '/' do
+    name = params[:name]
+    return "Posted '#{name}'"
+  end
+
+  get '/hello' do
+    NAME = params[:name]
+    return "Hello #{NAME}"
+  end
+
+  post "/submit" do
+    name = params[:name]
+    message = params[:message]
+
+    return "Thanks #{name}, you sent this message: '#{message}'"
+  end
+
+  get '/names' do
+    return "Julia, Mary, Karim"
+  end
+
+  post '/sort-names' do
+    names = params[:names].split(',')
+    return names.sort.join(',')
+  end
 end
---
+++ Sinatra/hello_web_project/spec/integration/app_spec.rb
@@ -0,0 +1,51 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+
+describe Application do
+  include Rack::Test::Methods
+
+  let(:app) { Application.new }
+
+  context "GET /names" do
+    it 'returns 200 OK' do
+      # Assuming the post with id 1 exists.
+      response = get('/names')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("Julia, Mary, Karim")
+
+    end
+
+    it 'returns 404 Not Found' do
+      response = get('/posts?id=276278')
+
+      expect(response.status).to eq(404)
+      # expect(response.body).to eq(expected_response)
+    end
+  end
+
+  context "POST /sort-names" do
+    it 'returns 200 OK' do
+      response = post('/sort-names', names: "Joe,Alice,Zoe,Julia,Kieran")
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("Alice,Joe,Julia,Kieran,Zoe")
+    end
+
+    it 'returns 200 OK with an empty string' do
+      response = post('/sort-names', names: "")
+
+      expect(response.status).to eq 200
+      expect(response.body).to eq ""
+    end
+
+    it 'returns 404 Not Found' do
+      response = get('/sort-names?id=276278')
+
+      expect(response.status).to eq(404)
+      expect(response.body).to eq("Sorry! We couldn't find this post.")
+    end
+  end
+end
